24089303f0a35deed3588443f5304a7dbb8fbe66
Sang wha Sung <sangwha0@naver.com>
2024-09-03 11:38:46 +0900

    Initial commit

---
+++ src/test/java/org/zerobase/scrapping/ScrappingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerobase.scrapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
